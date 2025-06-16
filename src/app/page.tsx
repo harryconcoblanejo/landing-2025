7ba2802d7b259e29e2713714d8ae028cd79e81af
harryconcoblanejo <harryconcoblanejo@gmail.com>
2025-06-16 12:26:23 -0300

    Add WhatsApp icon to contact section for improved accessibility

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,6 +8,7 @@ import { translations } from './translations';
 import ThemeToggle from './components/ThemeToggle';
 import { useForm, ValidationError } from '@formspree/react';
 import { FaWhatsapp, FaEnvelope, FaLinkedin } from 'react-icons/fa';
+import { SiWhatsapp } from 'react-icons/si';
 import { useTheme } from './contexts/ThemeContext';
 import Header from './components/Header';
 import ProjectCarousel from './components/ProjectCarousel';
@@ -161,7 +162,7 @@ export default function Home() {
               className="flex items-center gap-2 transition-colors"
               style={{ color: isDark ? '#25D366' : '#128C7E' }}
             >
-              <svg xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 24 24" className="w-5 h-5"><path d="M20.52 3.48A11.87 11.87 0 0 0 12 0C5.37 0 0 5.37 0 12c0 2.11.55 4.17 1.6 5.98L0 24l6.19-1.62A11.93 11.93 0 0 0 12 24c6.63 0 12-5.37 12-12 0-3.19-1.24-6.19-3.48-8.52zM12 22c-1.85 0-3.68-.5-5.25-1.44l-.38-.22-3.68.97.98-3.58-.25-.37A9.94 9.94 0 0 1 2 12C2 6.48 6.48 2 12 2c2.61 0 5.08 1.02 6.94 2.88A9.8 9.8 0 0 1 22 12c0 5.52-4.48 10-10 10zm5.13-7.47c-.28-.14-1.65-.81-1.9-.9-.25-.09-.43-.14-.61.14-.18.28-.28.7.9-.86 1.08-.16.18-.32.2-.6.07-.28-.14-1.18-.44-2.25-1.4-.83-.74-1.39-1.65-1.55-1.93-.16-.28-.02-.43.12-.57.13-.13.28-.34.42-.51.14-.17.18-.29.28-.48.09-.19.05-.36-.02-.5-.07-.14-.61-1.47-.84-2.01-.22-.53-.45-.46-.62-.47-.16-.01-.36-.01-.56-.01-.19 0-.5.07-.76.34-.26.27-1 1-.97 2.43.03 1.43 1.03 2.81 1.18 3.01.15.2 2.03 3.1 5.02 4.23.7.24 1.25.38 1.68.48.71.15 1.36.13 1.87.08.57-.06 1.65-.67 1.89-1.32.23-.65.23-1.2.16-1.32-.07-.12-.25-.19-.53-.33z"/></svg>
+              <SiWhatsapp className="w-5 h-5" />
               <span className="font-medium">+54 11 4163-6472</span>
             </a>
             {/* Mail */}
